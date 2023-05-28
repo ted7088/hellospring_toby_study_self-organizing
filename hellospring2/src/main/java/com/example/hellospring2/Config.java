542dae3ecab57fa878e367fef3cdbce0cc39ce37
ted7088 <ted3131@naver.com>
2023-05-28 15:03:46 +0900

    commit

--- hellospring2/src/main/java/com/example/hellospring2/Config.java
+++ hellospring2/src/main/java/com/example/hellospring2/Config.java
@@ -1,22 +1,20 @@
 package com.example.hellospring2;
 
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.swing.*;
-
-@MySpringBootAnnotaion
-public class Hellospring2Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Hellospring2Application.class,args);
-	}
-
+@Configuration
+public class Config {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
 }
